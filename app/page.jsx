d59c976e31ff1d46d564a3c93b61515fa13557e8
LeanBow <theleanbow@gmail.com>
2023-06-14 10:44:13 +0545

    add new files

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className="head_text text-center">
         Discover and Share
         <br className="max-md:hidden" />
-        <span className="orange_gradient text-center ">AI Powered Prmopts</span>
+        <span className="orange_gradient text-center ">AI Powered Prompts</span>
       </h1>
       <p className="desc text-center">
         Promtopia is an open-source AI prompting tool for modern world to
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,14 @@
 "use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   const { providers, setProviders } = useState(null);
 
@@ -33,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -69,19 +72,65 @@ const Nav = () => {
           </>
         )}
       </div>
+      {/* Mobile navigation  */}
+      <div className="sm:hidden flex relative">
+        {session?.user ? (
+          <div className="flex">
+            <Image
+              src="/assests/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
 
-      {/* MObile navigation */}
-      <div className="sm:hidden flex relative">{isUserLoggedIn?(
-        <div className="flex">
-                        <Image
-                src="/assests/images/logo.svg"
-                width={37}
-                height={37}
-                className="rounded-full"
-                alt="profile"
-              ></Image>
-        </div>
-      )}</div>
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full
+                  black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
